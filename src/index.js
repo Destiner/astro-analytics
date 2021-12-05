f96b1622b2fd7075a2e0a713bd50c3d8c7c46f30
Timur Badretdinov <destinerx@gmail.com>
2021-12-05 10:40:47 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Fathom from './Fathom.astro';
+import GoogleAnalytics from './GoogleAnalytics.astro';
+import Metrical from './Metrical.astro';
+import SimpleAnalytics from './SimpleAnalytics.astro';
+
+export {
+	Fathom,
+	GoogleAnalytics,
+	Metrical,
+	SimpleAnalytics,
+};
